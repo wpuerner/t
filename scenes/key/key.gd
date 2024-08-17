290fac3ffe5007e07d4bd0a8f4a6efa164a72aeb
wpuerner <williampuerner@gmail.com>
2024-08-17 08:29:58 -0400

    add key mask

--- scenes/key/key.gd
+++ scenes/key/key.gd
@@ -4,7 +4,7 @@ extends Node2D
 @export var key_sprite_texture: Resource:
 	set(value):
 		key_sprite_texture = value
-		$KeySprite.texture = key_sprite_texture
+		find_child("KeySprite").texture = key_sprite_texture
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
